65f788f6b7aa652bb19b8879c243dba173444710
Ehsan Mahmoodi <misaf.1990@gmail.com>
2026-07-06 23:21:27 +0330

    Update _ide_helper.php

--- _ide_helper.php
+++ _ide_helper.php
@@ -2,23 +2,23 @@
 
 namespace Filament\Forms\Components {
     /**
-     * @method static \Filament\Forms\Components\DateTimePicker jalali()
+     * @method static \Filament\Forms\Components\DateTimePicker jalali(bool $weekdaysShort = true)
      */
     class DateTimePicker {}
 }
 
 namespace Filament\Tables\Columns {
     /**
-     * @method static \Filament\Tables\Columns\TextColumn jalaliDate(string | \Closure | null $format = null, ?string $timezone = null)
-     * @method static \Filament\Tables\Columns\TextColumn jalaliDateTime(string | \Closure | null $format = null, ?string $timezone = null)
+     * @method static \Filament\Tables\Columns\TextColumn jalaliDate(string | \Closure | null $format = null, ?string $timezone = null, ?bool $latinNumbers = null)
+     * @method static \Filament\Tables\Columns\TextColumn jalaliDateTime(string | \Closure | null $format = null, ?string $timezone = null, ?bool $latinNumbers = null)
      */
     class TextColumn {}
 }
 
 namespace Filament\Infolists\Components {
     /**
-     * @method static \Filament\Infolists\Components\TextEntry jalaliDate(string | \Closure | null $format = null, ?string $timezone = null)
-     * @method static \Filament\Infolists\Components\TextEntry jalaliDateTime(string | \Closure | null $format = null, ?string $timezone = null)
+     * @method static \Filament\Infolists\Components\TextEntry jalaliDate(string | \Closure | null $format = null, ?string $timezone = null, ?bool $latinNumbers = null)
+     * @method static \Filament\Infolists\Components\TextEntry jalaliDateTime(string | \Closure | null $format = null, ?string $timezone = null, ?bool $latinNumbers = null)
      */
     class TextEntry {}
 }
